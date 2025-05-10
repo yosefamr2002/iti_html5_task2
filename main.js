f29e05a62ff74a732e14a9c7e9c883bb68565c17
YoussefTrust <yosefamr2002@icloud.com>
2025-05-10 21:59:41 +0300

    without array or innerhtml

--- main.js
+++ main.js
@@ -1,61 +1,49 @@
-var textVal  = document.querySelector("#textVal");
 var btn = document.querySelector(".btn");
-var inProgress = document.querySelector(".tasksSection");
+var progress = document.querySelector("#progress");
+// console.log(movers);
 
-var arr = []
-
-btn.addEventListener("click",function(){
-    arr.push(textVal.value);
-    console.log(arr);
-   display() 
-
- 
-})
+var counter = 1;
+var selected;
 
 
 
-// 1
+btn.addEventListener("click", function () {
+  var project = document.querySelector("#textVal");
+  var newli = document.createElement("li");
+  newli.innerHTML = project.value;
+  newli.classList = "movers";
+  newli.id = counter;
+  counter++;
+  newli.draggable = "true";
 
 
+  progress.appendChild(newli);
+  //   console.log(newli);
 
-function display (){
-    var cartona = ``;    
-  for(let i =0 ; i<=arr.length-1 ; i++){ 
-    cartona+= `<div class= "ulx" draggable="true" id="tasktitle${i}">
-                    <li >${arr[i]}</li>
-                </div>`; }
-inProgress.innerHTML=cartona;
-addeventtoUl()
-}
+  ///////////////////////////
 
-function addeventtoUl(){
+  newli.addEventListener("dragstart", function (e) {
+    selected = e.target;
+    console.log(selected);
 
-var ulx = document.querySelectorAll(".ulx");
-for(var i = 0 ; i < ulx.length ; i++){
-    ulx[i].addEventListener("dragstart",dragstartFn)
-}
+    var tasksSection = document.querySelectorAll(".tasksSection");
+    for (let i = 0; i < tasksSection.length; i++) {
 
-}
-//////////////////////////
+      tasksSection[i].addEventListener("dragover", function (e) {
+        e.preventDefault();
+      });
 
-var tasksSection = document.querySelectorAll(".tasksSection");
-
-for(let i =0 ; i<tasksSection.length ; i++){
-    tasksSection[i].addEventListener("dragover",dragoverFn)
-    tasksSection[i].addEventListener('drop',dropFn)
-}
+      tasksSection[i].addEventListener("drop", function (e) {
+        e.target.appendChild(selected);
+      });
+      
 
 
-function dragstartFn(e){
-    e.dataTransfer.setData('text', e.target.id)
-}
+    }
+  });
 
+  //remove input data
+  document.getElementById("textVal").value = "";
 
-function dragoverFn (e){
-    e.preventDefault();
-}
-
-function dropFn(e){
-    var dragedItem = document.getElementById(e.dataTransfer.getData('text'));
-    this.appendChild(dragedItem);
-}
+  
+});
